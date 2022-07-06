6b671fb2526452dc999dd1f08ebfb4a78824c444
sashavining <96848086+sashavining@users.noreply.github.com>
2022-07-06 19:21:16 -0400

    Basic user auth

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ const express = require('express')
 const router = express.Router()
 const Plant = require('../models/plant')
 const cloudinary = require('../utils/cloudinary')
+const User = require('../models/user')
+const passport = require('passport');
+const bcrypt = require('bcrypt')
 
 
 router.get('/', async (req, res) => {
@@ -25,6 +28,46 @@ router.get('/', async (req, res) => {
     res.render('index', { plants: plants, plantsImageTags: plantsImageTags })
 });
 
+router.get("/sign-up", (req, res) => res.render("sign-up-form"));
+router.get("/log-in", (req, res) => res.render("log-in-form"));
+
+router.post("/sign-up", (req, res, next) => {
+    bcrypt.hash("somePassword", 10, (err, hashedPassword) => {
+        if (err) {
+            console.log(err)
+            res.redirect("/sign-up")
+        } else {
+            const user = new User({
+                username: req.body.username,
+                password: hashedPassword
+              }).save(err => {
+                if (err) { 
+                  return next(err);
+                }
+                res.redirect("/");
+              });          
+        }
+      });
+  });
+
+router.post(
+    "/log-in",
+    passport.authenticate("local", {
+      successRedirect: "/",
+      failureRedirect: "/"
+    })
+  );
+
+router.get("/log-out", (req, res) => {
+    req.logout(function (err) {
+      if (err) {
+        return next(err);
+      }
+      res.redirect("/");
+    });
+  });
+  
+  
 async function createImageTag (publicId) {
     // Create an image tag with transformations applied to the src URL
     let imageTag = cloudinary.image(publicId, {
--- server.js
+++ server.js
@@ -4,7 +4,11 @@ if (process.env.NODE_ENV !== 'production') {
 
 const cloudinary = require('cloudinary').v2
 const express = require('express')
+const path = require("path");
 const app = express()
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 const expressLayouts = require('express-ejs-layouts')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
@@ -12,6 +16,7 @@ const methodOverride = require('method-override')
 
 const indexRouter = require('./routes/index')
 const plantRouter = require('./routes/plants')
+const User = require('./models/user')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -21,6 +26,45 @@ app.use(express.static('public'))
 app.use(methodOverride('_method'))
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true }));
+
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username }, (err, user) => {
+      if (err) { 
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, { message: "Authentication Failed" });
+      }
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          // passwords match! log user in
+          return done(null, user)
+        } else {
+          // passwords do not match!
+          return done(null, false, { message: "Authentication Failed" })
+        }
+      })      
+      return done(null, user);
+    });
+  })
+);
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
 
 
 const mongoose = require('mongoose')
